a136994def2bf15df5c969faa0d315855c240b33
Miguel Barcelo <miguel.barcelo@gmail.com>
2025-07-09 14:25:52 -0700

    feat: init project

---
+++ src/main/java/com/miguel_barcelo/async_price_finder/AsyncPriceFinderApplication.java
@@ -0,0 +1,13 @@
+package com.miguel_barcelo.async_price_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncPriceFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncPriceFinderApplication.class, args);
+	}
+
+}
